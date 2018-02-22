d7e841f4ede84f231ae050d21c32c7a1d886f7be
X. Sun <xs264@ew110.eng.cam.ac.uk>
2018-02-22 15:36:06 +0000

    motor testing

--- test_motors/identify_motors.cc
+++ test_motors/identify_motors.cc
@@ -3,36 +3,53 @@ using namespace std;
 #include <robot_instr.h>
 #include <robot_link.h>
 #include <delay.h>
-#define ROBOT_NUM 14                       // The id number (see below)
-robot_link  rlink;                            // datatype for the robot link
+#define ROBOT_NUM 14						// The id number (see below)
+robot_link  rlink;							// datatype for the robot link
 int main ()
 {
-rlink.initialise(ROBOT_NUM);
+#ifdef __arm__
+	rlink.initialise ();					//setup for local hardware
+#else
+	rlink.initialise(ROBOT_NUM);			//setup the link
+#endif
 
-rlink.command (MOTOR_1_GO,0);
-rlink.command (MOTOR_2_GO,0);
-rlink.command (MOTOR_3_GO,0);
-rlink.command (MOTOR_4_GO,0);
+rlink.command(WRITE_PORT_7, 255);
+
+rlink.command (RAMP_TIME,200);
+
+rlink.command (MOTOR_1_GO,100);
+rlink.command(WRITE_PORT_7, 1);
+delay(2000);
 int speed=rlink.request(MOTOR_1);
-int speed2=rlink.request(MOTOR_2);
-int speed3=rlink.request(MOTOR_3);
-int speed4=rlink.request(MOTOR_4);
 cout << speed << endl;
-cout << speed2 << endl;
-cout << speed3 << endl;
-cout << speed4 << endl;
+rlink.command (MOTOR_1_GO,0);
 
 
 rlink.command (MOTOR_2_GO,100);
-
+rlink.command(WRITE_PORT_7, 2);
 delay(2000);
-speed=rlink.request(MOTOR_2);
-
-delay(2000);
+int speed2=rlink.request(MOTOR_2);
+cout << speed2 << endl;
 rlink.command (MOTOR_2_GO,0);
 
-cout << speed << endl;
+
+rlink.command (MOTOR_3_GO,100);
+rlink.command(WRITE_PORT_7, 3);
+int speed3=rlink.request(MOTOR_3);
+delay(2000);
+cout << speed3 << endl;
+rlink.command (MOTOR_3_GO,0);
 
 
+rlink.command (MOTOR_4_GO,100);
+rlink.command(WRITE_PORT_7, 4);
+int speed4=rlink.request(MOTOR_4);
+delay(2000);
+cout << speed4 << endl;
+
+
+rlink.command (MOTOR_4_GO,0);
+
+rlink.command(WRITE_PORT_7, 255);
 return 0;
 }
