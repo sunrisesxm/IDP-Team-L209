86ff8935909f07f32f5c1f2b3f09e219183fafcf
Lawrence Berry <lawrence@sjc173n225.joh.private.cam.ac.uk>
2018-02-26 17:37:23 +0000

    Change name of DRAFT_1 to main

--- main/navigation.h
+++ main/navigation.h
@@ -8,7 +8,7 @@ class Navigation {
 public:
     LineSensors line_sensors;
     Motors motors;
-
+    // Publicly available methods and data
 private:
-  
+    // Internally used private methods and data
 };
