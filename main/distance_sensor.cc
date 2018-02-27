b2b0f2bffc82a57ef03064afac49b1ccf81a1ee6
Lawrence Berry <lawrence@dhcp-10-248-136-236.eduroam.wireless.private.cam.ac.uk>
2018-02-27 14:38:07 +0000

    Updates to main and new flowcharts

--- main/distance_sensor.cc
+++ main/distance_sensor.cc
@@ -9,3 +9,16 @@ extern robot_link rlink;
 #include "distance_sensor.h"
 
 // Distance sensor methods
+bool DistanceSensor::update();
+{
+    int byte = rlink.request(ADC0);
+    int ref = rlink.request(ADC1);
+    voltage = (double(byte)/double(ref))*reference; // Actual reference is 3.1V
+    distance = convert_voltage_to_distance(voltage);
+    
+}
+
+double DistanceSensor::convert_voltage_to_distance()
+{
+    // Calibration required to get the distance corresponding to voltage
+}
--- main/distance_sensor.h
+++ main/distance_sensor.h
@@ -1,3 +1,13 @@
 using namespace std;
 
 // Distance sensor class
+class DistanceSensor {
+public:
+    double distance;
+    bool update();
+private:
+    double voltage;
+    static const double reference = 3.1;
+    double convert_voltage_to_distance(double);
+
+}
